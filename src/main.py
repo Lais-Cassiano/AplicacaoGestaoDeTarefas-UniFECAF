50e079525f2a9be2c80a8f58aae931705b37452a
Lais S Cassiano <laiscassiano.101@gmail.com>
2026-01-12 17:05:32 -0300

    Implementação da  criação de tarefas

--- src/main.py
+++ src/main.py
@@ -18,6 +18,33 @@ def index():
     return render_template('index.html', tarefas=tarefas)
 
 
+# A linha abaixo cria uma rota para criar tarefas
+@app.route('/criar', methods=['GET', 'POST'])
+def criar_tarefa():
+    global proximo_id
+
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        descricao = request.form['descricao']
+        prioridade = request.form['prioridade']
+        status = request.form['status']
+
+        tarefa = {
+            "id": proximo_id,
+            "titulo": titulo,
+            "descricao": descricao,
+            "prioridade": prioridade,
+            "status": status
+        }
+
+        tarefas.append(tarefa)
+        proximo_id += 1
+
+        return redirect(url_for('index'))
+
+    return render_template('criar.html')
+
+
 # A linha abaixo mantem a aplicação executando 
 if __name__ == '__main__':
     app.run(debug=True)
